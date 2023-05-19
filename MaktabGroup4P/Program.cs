da77db4d76e5f376d28b2442dac52723549798e0
Danial Motevali <danial.motevali.82@gmail.com>
2023-05-19 17:07:28 +0330

    we have finished p2

--- MaktabGroup4P/Program.cs
+++ MaktabGroup4P/Program.cs
@@ -10,7 +10,11 @@ namespace MaktabGroup4P
             SalaryService salary = new SalaryService();
             int personID = 0;
             int year =0 ;
-            int month = 0; 
+            int month = 0;
+            string? solutionFolderPath = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)?.Parent?.Parent?.Parent?.FullName;
+            string path = Path.Combine(solutionFolderPath, "TotalSalary.txt");
+
+            StreamWriter sw = new StreamWriter(path);
 
             Console.Write("Please give your ID: \n- ");
             personID =Convert.ToInt32 (Console.ReadLine());
@@ -21,13 +25,14 @@ namespace MaktabGroup4P
             Console.Write("Please give your Month: \n- ");
             month = Convert.ToInt32(Console.ReadLine());
 
-           
 
             foreach (var item in salary.PaySlipByIdForOneMonth(personID,month,year))
             {
                 Console.WriteLine($"Id Number is {item.Id} and the name is {item.Name}, The tax is {item.Tax} and amount of income is {item.Amount} and otherBenefits are {item.OtherBenefit}. ");
             }
 
+            sw.WriteLine(salary.GetTotalSalaryById(personID));
+            sw.Close();
         }
     }
 }
