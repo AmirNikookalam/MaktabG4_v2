3bd9b94178638487b3cb1147ab9918e854cf8380
hossein <hosseinmirbagheri78@gmail.com>
2023-05-19 15:21:58 +0430

    IEmployeeService and EmployeeService has been created

---
+++ MaktabGroup4P/InterFace/IEmployeeService.cs
@@ -0,0 +1,15 @@
+﻿using MaktabGroup4P.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaktabGroup4P.InterFace
+{
+    public interface IEmployeeService
+    {
+        List<EmployeeNameViewModel> GetEmployeeName();
+        List<EmployeeNameViewModel> GetMenEmpName();
+    }
+}
---
+++ MaktabGroup4P/Services/EmployeeServicecs.cs
@@ -0,0 +1,27 @@
+﻿using MaktabGroup4P.InterFace;
+using MaktabGroup4P.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaktabGroup4P.Services
+{
+    public class EmployeeServicecs : IEmployeeService
+    {
+        AppDB dataBase = new AppDB();
+        public List<EmployeeNameViewModel> GetEmployeeName()
+        {
+            var empNames = from db in dataBase.employees select new EmployeeNameViewModel() {Fname = db.Fname,Lname = db.Lname,Id = db.Id};
+            return empNames.ToList();
+        }
+
+        public List<EmployeeNameViewModel> GetMenEmpName()
+        {
+            var empMenNames = from db in dataBase.employees select new EmployeeNameViewModel() { Fname = db.Fname, Lname = db.Lname, Id = db.Id };
+            return empMenNames.ToList();
+        }
+         
+    }
+}
