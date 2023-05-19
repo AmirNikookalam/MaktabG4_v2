a27de700e59905ca393ffbe630c4246060e5daf5
hossein <hosseinmirbagheri78@gmail.com>
2023-05-19 12:06:53 +0430

    ISalaryService has been added and PaySlipViewModel created

---
+++ MaktabGroup4P/ViewModel/PaySlipViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaktabGroup4P.ViewModel
+{
+    public class PaySlipViewModel
+    {
+    }
+}
